21398af0b8d476908115eb2983d689d033ce43e0
Cliff W <cliff.r.wong@gmail.com>
2016-12-07 15:42:45 -0500

    commit

--- house_robber3.py
+++ house_robber3.py
@@ -18,15 +18,10 @@ class Solution(object):
         return self.max
     
     def treeDFS(self, node):
-        accGrandLeft, accLeft = self.treeDFS(node.left) \
-                                        if node.left else (0, 0)
-        accGrandRight, accRight = self.treeDFS(node.right) \
-                                        if node.right else (0, 0)
-        accum = max([accGrandLeft+accGrandRight + node.val, \
-                    accLeft+accRight, \
-                    accGrandRight+accLeft, \
-                    accGrandLeft+accRight])
+        accGrandLeft, accLeft = self.treeDFS(node.left) if node.left else (0, 0)
+        accGrandRight, accRight = self.treeDFS(node.right) if node.right else (0, 0)
+        accum = max([accGrandLeft+accGrandRight + node.val, accLeft+accRight, \
+                    accGrandRight+accLeft, accGrandLeft+accRight])
         if accum > self.max:
             self.max = accum
         return (accLeft + accRight, accum)
-        
