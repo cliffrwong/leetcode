9fc397c7424615a543521f93829a51849dad14bc
Cliff W <cliff.r.wong@gmail.com>
2016-12-07 15:19:02 -0500

    first commit

---
+++ house_robber3.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.accum = 0
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def rob(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        self.max = 0
+        self.treeDFS(root)
+        return self.max
+    
+    def treeDFS(self, node):
+        maxLeftGChild, maxLeftChild = (self.treeDFS(node.left), node.left.accum) \
+                                        if node.left else (0, 0)
+        maxRightGChild, maxRightChild = (self.treeDFS(node.right), node.right.accum) \
+                                        if node.right else (0, 0)
+        
+        node.accum = max([maxLeftGChild+maxRightGChild + node.val, \
+                        maxLeftChild+maxRightChild, \
+                        maxRightGChild+maxLeftChild, \
+                        maxLeftGChild+maxRightChild])
+        if node.accum > self.max:
+            self.max = node.accum
+            
+        leftVal = node.left.accum if node.left else 0
+        rightVal = node.right.accum if node.right else 0
+        return leftVal + rightVal
