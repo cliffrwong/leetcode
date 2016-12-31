e0acb33fb6960ba5a301faf273e05d140be08284
Cliff W <cliff.r.wong@gmail.com>
2016-12-31 17:01:43 -0500

    added counting bits

--- 338_Counting Bits.py
+++ 338_Counting Bits.py
@@ -1,4 +1,4 @@
-import math
+from math import ceil, floor
 
 class Solution(object):
     def countBits(self, num):
@@ -11,12 +11,12 @@ class Solution(object):
         accList = [[0],[1]]
         numBits = math.log(num+1,2)
         
-        for i in range(2, int(math.ceil(numBits)+1)):
+        for i in range(2, int(ceil(numBits)+1)):
             list2 = accList[-1] + [x+1 for x in accList[-1]]
             if i <= numBits:
                 accList.append(list2)
             else:
-                accList.append(list2[:int(num+1-2**math.floor(numBits))])
+                accList.append(list2[:int(num+1-2**floor(numBits))])
         # Flatten list of lists
         return [item for sublist in accList for item in sublist]
             
