31f1e4747d44d3c25c279dbec73fc0869b13813a
Cliff W <cliff.r.wong@gmail.com>
2017-01-22 13:35:02 -0500

    added 413 and 487

--- 413_arith_slices.py
+++ 413_arith_slices.py
@@ -1,28 +1,18 @@
 class Solution(object):
  
-    def lookupFunc(self, n):
-        if n not in self.lookup:
-            self.lookup[n] = -self.lookupFunc(n-2) + 2*self.lookupFunc(n-1) + 1
-        return self.lookup[n]
-            
     def numberOfArithmeticSlices(self, A):
         """
         :type A: List[int]
         :rtype: int
         """
-        self.lookup = {3:1, 4:3}
         slices, accumLen, accumDiff = 0, 0, None
-        
         for idx in range(1, len(A)):
             curDiff = A[idx]-A[idx-1]
             if curDiff == accumDiff:
                 accumLen += 1
+                slices += accumLen
             else:
-                if accumLen >= 3:
-                    slices += self.lookupFunc(accumLen)
-                accumLen = 2
+                accumLen = 0
                 accumDiff = curDiff
-        if accumLen >= 3:
-            slices += self.lookupFunc(accumLen)
         return slices
         
